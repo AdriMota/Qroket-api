2f6f3d60ec0bff403b46bc390e0333b5f6fd0642
Adriana Mota <adrianamota17@hotmail.com>
2022-10-10 16:28:51 +0200

    Connect to a MongoDB database with Mongoose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/your-app-name');
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/qroket');
 
 const app = express();
 
